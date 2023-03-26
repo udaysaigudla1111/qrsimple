80d4ee112d6f6b94a0e71b00cc26f6f546efecfc
GUDLA UDAY SAI <udaysaigudla1111@gmail.com>
2023-03-26 23:14:12 +0530

    gpt

--- backend/server.js
+++ backend/server.js
@@ -123,8 +123,8 @@ async function connect() {
 }
 
 connect();
-
-app.listen(8000, () => {
-    console.log("Server started on port 8000");
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log("Server started on port ${port}");
 });
 
